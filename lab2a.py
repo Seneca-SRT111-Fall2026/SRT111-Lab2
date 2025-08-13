8aacab6504db9ede3a8c4f93e69629e308c73e78
Tiayyba Riaz <121324609+tiayyba@users.noreply.github.com>
2025-08-12 23:09:57 -0400

    Update lab2a.py

--- lab2a.py
+++ lab2a.py
@@ -3,7 +3,7 @@
 #!/usr/bin/env python3
 # Author:
 # Date:
-# Purpose: Create a variable, check its type and use a condition to check the value fo the variable.
+# Purpose: Create a variable, check its type and use a condition to check the value of the variable.
 # Usage: ./lab2a.py
 
 # TO DO 1: Follow the instructions given in README.md file
