2d019840228516f7720a698bedac12496ce3a316
Tiayyba Riaz <121324609+tiayyba@users.noreply.github.com>
2026-01-15 15:47:58 -0500

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,9 +1,4 @@
-# Add comments before you do anything else.
-
-#!/usr/bin/env python3
 # Author:
 # Date:
 # Purpose: Create a variable, check its type and use a condition to check the value of the variable.
 # Usage: ./task1.py
-
-# TO DO 1: Follow the instructions given in README.md file
